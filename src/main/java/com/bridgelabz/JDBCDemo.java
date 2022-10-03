5f307da078a62b72b0daa8e018feb00997185c84
SAHIL0455 <sahilpawar1004@gmail.com>
2022-10-03 19:14:51 +0530

    ADDED: connet to database

--- src/main/java/com/bridgelabz/JDBCDemo.java
+++ src/main/java/com/bridgelabz/JDBCDemo.java
@@ -1,9 +1,29 @@
 package com.bridgelabz;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Enumeration;
 
 
 public class JDBCDemo {
     public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to database");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?userSSL=false";
+        String name = "root";
+        String password = "Sahil@6436";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver is Loaded");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        try {
+            connection = DriverManager.getConnection(jdbcURL,name,password);
+            System.out.println("connection is successfully");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
