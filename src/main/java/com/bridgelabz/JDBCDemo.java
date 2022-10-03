418035c2ed85c72c6894513b98a292eae228e714
SAHIL0455 <sahilpawar1004@gmail.com>
2022-10-03 19:41:46 +0530

    ADDED: using the prepared statement

--- src/main/java/com/bridgelabz/JDBCDemo.java
+++ src/main/java/com/bridgelabz/JDBCDemo.java
@@ -7,9 +7,10 @@ public class JDBCDemo {
     public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to database");
         Connection connection = DatabaseConnection.connection();
-        Statement statement = connection.createStatement();
-        statement.execute("update payroll_service.employee_payroll set salary = 300000 where name = 'sahil'");
-        ResultSet resultSet = statement.executeQuery("select * from payroll_service.employee_payroll");
+        PreparedStatement preparedStatement = connection.prepareStatement("update payroll_service.employee_payroll set salary = 300005 where name = 'sahil'");
+        preparedStatement.execute();
+        ResultSet resultSet = preparedStatement.executeQuery("select * from payroll_service.employee_payroll");
+        System.out.println("update salary in Employee Payroll service");
         System.out.println("Retrive the Employee Payroll");
         while (resultSet.next()) {
             System.out.println(resultSet.getInt("id") + " " + resultSet.getString("name")
